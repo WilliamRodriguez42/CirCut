4f965195f09c0c48503c8fd9c14230c1f0ea43f2
Austin Kee <austinjkee@gmail.com>
2019-07-17 17:20:46 -0400

    Created the outline for the C implementation of the Christofides algorithm.

---
+++ server/gerber_to_gcode/christofides.c
@@ -0,0 +1,39 @@
+#include <Python.h>
+#include <stdint.h>
+
+struct Route{
+    int_64t length
+    int_64t** path
+};
+
+Route tsp(int_64t** data){
+    int_64t** graph;
+    int_64t** minTree;
+    int_64t** oddVertices;
+    int_64t** eulerTour;
+
+
+
+    return eulerTour;
+}
+
+/**
+*   Finds the minimum spanning tree of a graph using Krustal's
+*   @param graph - Some graph that exists in a 2D plane
+*   @return mst - A minimum spanning tree
+**/
+int_64t** minimumSpanningTree(int_64t** graph){
+    return graph;
+}
+
+int_64t** findOddVertices(int_64t** tree){
+    return tree;
+}
+
+int_64t** minimumWeightMatch(int_64t** tree, int_64t** graph, int_64t** oddVertices){
+    return tree;
+}
+
+int_64t** findEulerTour(int_64t** tree, int_64t** graph){
+    return tree;
+}
